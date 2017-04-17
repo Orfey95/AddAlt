1825b93ffb679d8af93efaa49caf21e0b9b1e7c7
Sasha <sasha7692@gmail.com>
2017-04-17 19:33:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,56 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-     static List<String> workFile;
+    static List<String> workFile;
     public static void main(String[] args) throws IOException {
 
         ReadMDFile();
-        System.out.println(workFile.get(20));
-        SaveMDFile();
+        //System.out.println(workFile.get(20));
+        SearchImagesInMDFile();
+        //SaveMDFile();
+        /*String x = "![Image]({{ site.url }}{% link /assets/images/how-to/search-project-gerrit.png %})";
+        if (CheckStr(x)){
+            System.out.println("+");
+        }*/
+
     }
 
-   static void ReadMDFile()throws IOException {
+    static void ReadMDFile()throws IOException {
         String fileName = "src\\testFile.md";
-       workFile = new ArrayList<>(Files.readAllLines(Paths.get(fileName)));
+        workFile = new ArrayList<>(Files.readAllLines(Paths.get(fileName)));
     }
-	void SearchImagesInMDFile(){
-	;
-	}
-	void EditMDFile(){
-	;
-	}
-	  static void SaveMDFile() throws IOException {
+    static String SearchImagesInMDFile(){
+        String currentStr = "";
+        String strImg = "";
+        for (int i = 0; i < workFile.size(); i++){
+            currentStr = workFile.get(i);
+            if (CheckStr(currentStr)){
+                strImg = currentStr;
+                System.out.println(strImg);
+                return strImg;
+            }
+        }
+        return "0";
+    }
+
+    static boolean CheckStr(String currentStr) {
+        Pattern p = Pattern.compile("^(.+|)\\!\\[.+\\]\\(.+\\)");
+        Matcher m = p.matcher(currentStr);
+        return m.matches();
+    }
+
+    void EditMDFile(){
+        ;
+    }
+
+    static void SaveMDFile() throws IOException {
         Files.write(Paths.get("src\\out.md"), workFile);
     }
 }
