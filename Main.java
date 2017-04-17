9b1209f47f2acdceb10ab877832d49f393424be0
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2017-04-17 16:28:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,4 +3,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+	void ReadMDFile(){
+	;
+    }
+	void SearchMDFile(){
+	;
+	}
+	void EditMDFile(){
+	;
+	}
+	void SaveMDFile(){
+	    ;
+	}
 }
