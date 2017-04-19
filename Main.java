7c821cfba1d0bae3a13a4f2b5f9237a3fa9646d5
Sasha <sasha7692@gmail.com>
2017-04-19 20:57:56 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,11 +10,10 @@ public class Main {
 
     static List<String> workFile;
     public static void main(String[] args) throws IOException {
-
-        ReadMDFile();
-        //System.out.println(workFile.get(20));
-        SearchImagesInMDFile();
-        //SaveMDFile(); 
+        //ReadMDFile();
+        //SearchImagesInMDFile();
+        //EditMDFile(SearchImagesInMDFile());
+        //SaveMDFile();
     }
 
     static void ReadMDFile()throws IOException {
@@ -42,7 +41,31 @@ public class Main {
     }
 
     void EditMDFile(){
-        ;
+        Pattern p = Pattern.compile("\\!\\[(.+)\\](.+)");
+        Matcher m = p.matcher(tempStr);
+        if (m.matches()) {
+            System.out.println(m.group(1));
+        } else {
+            System.out.println("Error");
+        }
+
+        Pattern p1 = Pattern.compile("(\\/).+(\\.)");
+        Matcher m1 = p1.matcher(tempStr);
+        String a = "";
+        if(m1.find()){
+            a = m1.group();
+            a = a.substring(0, a.length()-1);
+            String[] am = a.split("/");
+            a = am[am.length-1];
+            a = a.replace('-', ' ');
+            a = a.substring(0, 1).toUpperCase() + a.substring(1);
+            System.out.println(a);
+        } else {
+            System.out.println("Error");
+        }
+        if(m.group(1) != a) {
+            tempStr = tempStr.replaceAll(m.group(1), a);
+        }
     }
 
     static void SaveMDFile() throws IOException {
